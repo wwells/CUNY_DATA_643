852d1f86712df8c4dbe8a99c029af287b980ea76
wwells <walt@occ-data.org>
2017-06-09 16:31:06 -0500

    feat(P1): ALT.py fin

--- Project1/P1_ALT.py
+++ Project1/P1_ALT.py
@@ -16,7 +16,7 @@ users = ['User_' + s for s in LETTERS[0:n]]
 items = ['Item_' + s for s in LETTERS[0:n]]
 
 #create initial df
-df = pd.DataFrame(np.random.randint(1,6,size=(n, n)), columns=items)
+df = pd.DataFrame(np.random.randint(1,6,size=(n, n)), columns=items, index=users)
 
 #insert randoms vars
 ## https://stackoverflow.com/questions/42091018/randomly-insert-nas-values-in-a-pandas-dataframe-with-no-rows-completely-miss?rq=1
@@ -37,6 +37,22 @@ testRMSE = ((test.stack() - train_raw_avg) ** 2).mean() ** .5
 
 user_bias_train = train.mean(1) - train_raw_avg
 user_bias_test = test.mean(1) - train_raw_avg
-item_bias_test = test.mean() - train_raw_avg
+item_bias_train = train.mean() - train_raw_avg
 
 # make predictions based on these calculations
+train_prediction = [x + y + train_raw_avg for x in user_bias_train for y in item_bias_train]
+train_pred_df = pd.DataFrame(np.array(train_prediction).reshape(len(train), 15))
+
+test_prediction = [x + y + train_raw_avg for x in user_bias_test for y in item_bias_train]
+test_pred_df = pd.DataFrame(np.array(test_prediction).reshape(len(test), 15))
+
+trainPredRMSE = ((train_pred_df.stack() - train_raw_avg) ** 2).mean() ** .5
+testPredRMSE = ((test_pred_df.stack() - train_raw_avg) ** 2).mean() ** .5
+
+#Summary
+dict = [(trainRMSE, trainPredRMSE, (1-trainPredRMSE/trainRMSE)*100),
+        (testRMSE, testPredRMSE, (1-testPredRMSE/testRMSE)*100)]
+summary = pd.DataFrame(dict, 
+    columns = ["Raw Average RMSE", "Simple Predictor RMSE", "Percent Improvement"], 
+    index = ['train', 'test'])
+summary
